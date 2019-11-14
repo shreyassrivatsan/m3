1ca609273e3f402007b7f68781dfb21288c3de06
Shreyas Srivatsan <shreyas@chronosphere.io>
2019-11-13 20:39:23 -0800

    Update remote write proto

--- src/query/generated/proto/prompb/types.pb.go
+++ src/query/generated/proto/prompb/types.pb.go
@@ -65,8 +65,9 @@ func (x LabelMatcher_Type) String() string {
 func (LabelMatcher_Type) EnumDescriptor() ([]byte, []int) { return fileDescriptorTypes, []int{4, 0} }
 
 type Sample struct {
-	Value     float64 `protobuf:"fixed64,1,opt,name=value,proto3" json:"value,omitempty"`
-	Timestamp int64   `protobuf:"varint,2,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	Value     float64   `protobuf:"fixed64,1,opt,name=value,proto3" json:"value,omitempty"`
+	Timestamp int64     `protobuf:"varint,2,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	Exemplar  *Exemplar `protobuf:"bytes,3,opt,name=exemplar" json:"exemplar,omitempty"`
 }
 
 func (m *Sample) Reset()                    { *m = Sample{} }
@@ -88,6 +89,14 @@ func (m *Sample) GetTimestamp() int64 {
 	return 0
 }
 
+func (m *Sample) GetExemplar() *Exemplar {
+	if m != nil {
+		return m.Exemplar
+	}
+	return nil
+}
+
+// TimeSeries represents samples and labels for a single time series.
 type TimeSeries struct {
 	Labels  []Label  `protobuf:"bytes,1,rep,name=labels" json:"labels"`
 	Samples []Sample `protobuf:"bytes,2,rep,name=samples" json:"samples"`
@@ -113,8 +122,8 @@ func (m *TimeSeries) GetSamples() []Sample {
 }
 
 type Label struct {
-	Name  []byte `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
-	Value []byte `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
+	Name  string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Value string `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
 }
 
 func (m *Label) Reset()                    { *m = Label{} }
@@ -122,18 +131,18 @@ func (m *Label) String() string            { return proto.CompactTextString(m) }
 func (*Label) ProtoMessage()               {}
 func (*Label) Descriptor() ([]byte, []int) { return fileDescriptorTypes, []int{2} }
 
-func (m *Label) GetName() []byte {
+func (m *Label) GetName() string {
 	if m != nil {
 		return m.Name
 	}
-	return nil
+	return ""
 }
 
-func (m *Label) GetValue() []byte {
+func (m *Label) GetValue() string {
 	if m != nil {
 		return m.Value
 	}
-	return nil
+	return ""
 }
 
 type Labels struct {
@@ -155,8 +164,8 @@ func (m *Labels) GetLabels() []Label {
 // Matcher specifies a rule, which can match or set of labels or not.
 type LabelMatcher struct {
 	Type  LabelMatcher_Type `protobuf:"varint,1,opt,name=type,proto3,enum=m3prometheus.LabelMatcher_Type" json:"type,omitempty"`
-	Name  []byte            `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
-	Value []byte            `protobuf:"bytes,3,opt,name=value,proto3" json:"value,omitempty"`
+	Name  string            `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	Value string            `protobuf:"bytes,3,opt,name=value,proto3" json:"value,omitempty"`
 }
 
 func (m *LabelMatcher) Reset()                    { *m = LabelMatcher{} }
@@ -171,18 +180,50 @@ func (m *LabelMatcher) GetType() LabelMatcher_Type {
 	return LabelMatcher_EQ
 }
 
-func (m *LabelMatcher) GetName() []byte {
+func (m *LabelMatcher) GetName() string {
 	if m != nil {
 		return m.Name
+	}
+	return ""
+}
+
+func (m *LabelMatcher) GetValue() string {
+	if m != nil {
+		return m.Value
+	}
+	return ""
+}
+
+type Exemplar struct {
+	Labels    []Label `protobuf:"bytes,1,rep,name=labels" json:"labels"`
+	Value     float64 `protobuf:"fixed64,2,opt,name=value,proto3" json:"value,omitempty"`
+	Timestamp int64   `protobuf:"varint,3,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+}
+
+func (m *Exemplar) Reset()                    { *m = Exemplar{} }
+func (m *Exemplar) String() string            { return proto.CompactTextString(m) }
+func (*Exemplar) ProtoMessage()               {}
+func (*Exemplar) Descriptor() ([]byte, []int) { return fileDescriptorTypes, []int{5} }
+
+func (m *Exemplar) GetLabels() []Label {
+	if m != nil {
+		return m.Labels
 	}
 	return nil
 }
 
-func (m *LabelMatcher) GetValue() []byte {
+func (m *Exemplar) GetValue() float64 {
 	if m != nil {
 		return m.Value
 	}
-	return nil
+	return 0
+}
+
+func (m *Exemplar) GetTimestamp() int64 {
+	if m != nil {
+		return m.Timestamp
+	}
+	return 0
 }
 
 func init() {
@@ -191,6 +232,7 @@ func init() {
 	proto.RegisterType((*Label)(nil), "m3prometheus.Label")
 	proto.RegisterType((*Labels)(nil), "m3prometheus.Labels")
 	proto.RegisterType((*LabelMatcher)(nil), "m3prometheus.LabelMatcher")
+	proto.RegisterType((*Exemplar)(nil), "m3prometheus.Exemplar")
 	proto.RegisterEnum("m3prometheus.LabelMatcher_Type", LabelMatcher_Type_name, LabelMatcher_Type_value)
 }
 func (m *Sample) Marshal() (dAtA []byte, err error) {
@@ -218,6 +260,16 @@ func (m *Sample) MarshalTo(dAtA []byte) (int, error) {
 		dAtA[i] = 0x10
 		i++
 		i = encodeVarintTypes(dAtA, i, uint64(m.Timestamp))
+	}
+	if m.Exemplar != nil {
+		dAtA[i] = 0x1a
+		i++
+		i = encodeVarintTypes(dAtA, i, uint64(m.Exemplar.Size()))
+		n1, err := m.Exemplar.MarshalTo(dAtA[i:])
+		if err != nil {
+			return 0, err
+		}
+		i += n1
 	}
 	return i, nil
 }
@@ -359,6 +411,47 @@ func (m *LabelMatcher) MarshalTo(dAtA []byte) (int, error) {
 	return i, nil
 }
 
+func (m *Exemplar) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *Exemplar) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if len(m.Labels) > 0 {
+		for _, msg := range m.Labels {
+			dAtA[i] = 0xa
+			i++
+			i = encodeVarintTypes(dAtA, i, uint64(msg.Size()))
+			n, err := msg.MarshalTo(dAtA[i:])
+			if err != nil {
+				return 0, err
+			}
+			i += n
+		}
+	}
+	if m.Value != 0 {
+		dAtA[i] = 0x11
+		i++
+		binary.LittleEndian.PutUint64(dAtA[i:], uint64(math.Float64bits(float64(m.Value))))
+		i += 8
+	}
+	if m.Timestamp != 0 {
+		dAtA[i] = 0x18
+		i++
+		i = encodeVarintTypes(dAtA, i, uint64(m.Timestamp))
+	}
+	return i, nil
+}
+
 func encodeVarintTypes(dAtA []byte, offset int, v uint64) int {
 	for v >= 1<<7 {
 		dAtA[offset] = uint8(v&0x7f | 0x80)
@@ -376,6 +469,10 @@ func (m *Sample) Size() (n int) {
 	}
 	if m.Timestamp != 0 {
 		n += 1 + sovTypes(uint64(m.Timestamp))
+	}
+	if m.Exemplar != nil {
+		l = m.Exemplar.Size()
+		n += 1 + l + sovTypes(uint64(l))
 	}
 	return n
 }
@@ -437,6 +534,24 @@ func (m *LabelMatcher) Size() (n int) {
 	l = len(m.Value)
 	if l > 0 {
 		n += 1 + l + sovTypes(uint64(l))
+	}
+	return n
+}
+
+func (m *Exemplar) Size() (n int) {
+	var l int
+	_ = l
+	if len(m.Labels) > 0 {
+		for _, e := range m.Labels {
+			l = e.Size()
+			n += 1 + l + sovTypes(uint64(l))
+		}
+	}
+	if m.Value != 0 {
+		n += 9
+	}
+	if m.Timestamp != 0 {
+		n += 1 + sovTypes(uint64(m.Timestamp))
 	}
 	return n
 }
@@ -513,6 +628,39 @@ func (m *Sample) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Exemplar", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTypes
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthTypes
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Exemplar == nil {
+				m.Exemplar = &Exemplar{}
+			}
+			if err := m.Exemplar.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipTypes(dAtA[iNdEx:])
@@ -679,7 +827,7 @@ func (m *Label) Unmarshal(dAtA []byte) error {
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
 			}
-			var byteLen int
+			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowTypes
@@ -689,28 +837,26 @@ func (m *Label) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
+				stringLen |= (uint64(b) & 0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			if byteLen < 0 {
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
 				return ErrInvalidLengthTypes
 			}
-			postIndex := iNdEx + byteLen
+			postIndex := iNdEx + intStringLen
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Name = append(m.Name[:0], dAtA[iNdEx:postIndex]...)
-			if m.Name == nil {
-				m.Name = []byte{}
-			}
+			m.Name = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Value", wireType)
 			}
-			var byteLen int
+			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowTypes
@@ -720,22 +866,20 @@ func (m *Label) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
+				stringLen |= (uint64(b) & 0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			if byteLen < 0 {
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
 				return ErrInvalidLengthTypes
 			}
-			postIndex := iNdEx + byteLen
+			postIndex := iNdEx + intStringLen
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Value = append(m.Value[:0], dAtA[iNdEx:postIndex]...)
-			if m.Value == nil {
-				m.Value = []byte{}
-			}
+			m.Value = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
@@ -891,7 +1035,7 @@ func (m *LabelMatcher) Unmarshal(dAtA []byte) error {
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
 			}
-			var byteLen int
+			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowTypes
@@ -901,28 +1045,26 @@ func (m *LabelMatcher) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
+				stringLen |= (uint64(b) & 0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			if byteLen < 0 {
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
 				return ErrInvalidLengthTypes
 			}
-			postIndex := iNdEx + byteLen
+			postIndex := iNdEx + intStringLen
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Name = append(m.Name[:0], dAtA[iNdEx:postIndex]...)
-			if m.Name == nil {
-				m.Name = []byte{}
-			}
+			m.Name = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 3:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Value", wireType)
 			}
-			var byteLen int
+			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowTypes
@@ -932,23 +1074,132 @@ func (m *LabelMatcher) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				byteLen |= (int(b) & 0x7F) << shift
+				stringLen |= (uint64(b) & 0x7F) << shift
 				if b < 0x80 {
 					break
 				}
 			}
-			if byteLen < 0 {
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
 				return ErrInvalidLengthTypes
 			}
-			postIndex := iNdEx + byteLen
+			postIndex := iNdEx + intStringLen
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Value = append(m.Value[:0], dAtA[iNdEx:postIndex]...)
-			if m.Value == nil {
-				m.Value = []byte{}
+			m.Value = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTypes(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthTypes
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *Exemplar) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTypes
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: Exemplar: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: Exemplar: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Labels", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTypes
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthTypes
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Labels = append(m.Labels, Label{})
+			if err := m.Labels[len(m.Labels)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
 			}
 			iNdEx = postIndex
+		case 2:
+			if wireType != 1 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Value", wireType)
+			}
+			var v uint64
+			if (iNdEx + 8) > l {
+				return io.ErrUnexpectedEOF
+			}
+			v = uint64(binary.LittleEndian.Uint64(dAtA[iNdEx:]))
+			iNdEx += 8
+			m.Value = float64(math.Float64frombits(v))
+		case 3:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Timestamp", wireType)
+			}
+			m.Timestamp = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTypes
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Timestamp |= (int64(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := skipTypes(dAtA[iNdEx:])
@@ -1080,29 +1331,31 @@ func init() {
 }
 
 var fileDescriptorTypes = []byte{
-	// 369 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x52, 0xc1, 0x6a, 0xdb, 0x40,
-	0x10, 0xf5, 0x4a, 0xb2, 0x4c, 0xa7, 0xa6, 0x88, 0xad, 0x0f, 0xa2, 0x14, 0xd9, 0xe8, 0xa4, 0x4b,
-	0x25, 0x6c, 0xf5, 0xd6, 0x42, 0xc1, 0xa0, 0x9b, 0x1b, 0xb0, 0xec, 0x53, 0x6e, 0x92, 0x3d, 0x91,
-	0x05, 0x5a, 0x4b, 0xd1, 0xae, 0x02, 0xfe, 0x8b, 0xdc, 0xf2, 0x4b, 0x3e, 0xe6, 0x0b, 0x42, 0x70,
-	0x7e, 0x24, 0x68, 0xd7, 0x89, 0x1d, 0xf0, 0x25, 0x97, 0x65, 0xe6, 0xed, 0x7b, 0x6f, 0xde, 0x32,
-	0x0b, 0xff, 0xb2, 0x5c, 0x6c, 0x9a, 0xd4, 0x5f, 0x95, 0x2c, 0x60, 0xe1, 0x3a, 0x0d, 0x58, 0x18,
-	0xf0, 0x7a, 0x15, 0xdc, 0x36, 0x58, 0xef, 0x82, 0x0c, 0xb7, 0x58, 0x27, 0x02, 0xd7, 0x41, 0x55,
-	0x97, 0xa2, 0x6c, 0x4f, 0x56, 0xa5, 0x81, 0xd8, 0x55, 0xc8, 0x7d, 0x09, 0xd1, 0x3e, 0x0b, 0x5b,
-	0x14, 0xc5, 0x06, 0x1b, 0xfe, 0xe3, 0xd7, 0x99, 0x5d, 0x56, 0x66, 0xa5, 0xd2, 0xa5, 0xcd, 0x8d,
-	0xec, 0x94, 0x49, 0x5b, 0x29, 0xb1, 0xfb, 0x17, 0xcc, 0x45, 0xc2, 0xaa, 0x02, 0xe9, 0x00, 0xba,
-	0x77, 0x49, 0xd1, 0xa0, 0x4d, 0x46, 0xc4, 0x23, 0xb1, 0x6a, 0xe8, 0x4f, 0xf8, 0x22, 0x72, 0x86,
-	0x5c, 0x24, 0xac, 0xb2, 0xb5, 0x11, 0xf1, 0xf4, 0xf8, 0x04, 0xb8, 0x0d, 0xc0, 0x32, 0x67, 0xb8,
-	0xc0, 0x3a, 0x47, 0x4e, 0xc7, 0x60, 0x16, 0x49, 0x8a, 0x05, 0xb7, 0xc9, 0x48, 0xf7, 0xbe, 0x4e,
-	0xbe, 0xfb, 0xe7, 0xc9, 0xfc, 0x59, 0x7b, 0x37, 0x35, 0xf6, 0x4f, 0xc3, 0x4e, 0x7c, 0x24, 0xd2,
-	0xdf, 0xd0, 0xe3, 0x72, 0x3c, 0xb7, 0x35, 0xa9, 0x19, 0x7c, 0xd4, 0xa8, 0x6c, 0x47, 0xd1, 0x1b,
-	0xd5, 0x1d, 0x43, 0x57, 0x9a, 0x51, 0x0a, 0xc6, 0x36, 0x61, 0x2a, 0x72, 0x3f, 0x96, 0xf5, 0xe9,
-	0x1d, 0x9a, 0x04, 0x55, 0xe3, 0xfe, 0x01, 0x73, 0xa6, 0x46, 0x7e, 0x3e, 0xa5, 0xfb, 0x40, 0xa0,
-	0x2f, 0xf1, 0xff, 0x89, 0x58, 0x6d, 0xb0, 0xa6, 0x21, 0x18, 0xed, 0x06, 0xe4, 0xdc, 0x6f, 0x93,
-	0xe1, 0x05, 0x87, 0x23, 0xd3, 0x5f, 0xee, 0x2a, 0x8c, 0x25, 0xf9, 0x3d, 0xac, 0x76, 0x29, 0xac,
-	0x7e, 0x1e, 0xd6, 0x03, 0xa3, 0xd5, 0x51, 0x13, 0xb4, 0x68, 0x6e, 0x75, 0x68, 0x0f, 0xf4, 0xab,
-	0x68, 0x6e, 0x91, 0x16, 0x88, 0x23, 0x4b, 0x93, 0x40, 0x1c, 0x59, 0xfa, 0xd4, 0xde, 0x1f, 0x1c,
-	0xf2, 0x78, 0x70, 0xc8, 0xf3, 0xc1, 0x21, 0xf7, 0x2f, 0x4e, 0xe7, 0xda, 0x54, 0x3f, 0x24, 0x35,
-	0xe5, 0x7e, 0xc3, 0xd7, 0x00, 0x00, 0x00, 0xff, 0xff, 0x74, 0x54, 0xd9, 0x83, 0x5f, 0x02, 0x00,
-	0x00,
+	// 410 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x52, 0xc1, 0x6e, 0xd4, 0x30,
+	0x10, 0x5d, 0x27, 0xdb, 0xb4, 0x9d, 0x56, 0x28, 0x32, 0x15, 0x8a, 0x10, 0x4a, 0xa3, 0x9c, 0x72,
+	0x21, 0x51, 0x13, 0x6e, 0x1c, 0x90, 0x2a, 0xe5, 0x56, 0x90, 0xea, 0xf6, 0xc4, 0xcd, 0xd9, 0x0e,
+	0xd9, 0x48, 0x71, 0xe3, 0xc6, 0x0e, 0x62, 0xff, 0x82, 0x1b, 0xbf, 0xd4, 0x23, 0x5f, 0x80, 0xd0,
+	0xf2, 0x23, 0x28, 0x76, 0xda, 0xee, 0xa2, 0xbd, 0xd0, 0x8b, 0xe5, 0x79, 0x7e, 0x6f, 0xfc, 0xc6,
+	0x7e, 0xf0, 0xa1, 0x6e, 0xf4, 0x72, 0xa8, 0xd2, 0x45, 0x27, 0x32, 0x51, 0xdc, 0x54, 0x99, 0x28,
+	0x32, 0xd5, 0x2f, 0xb2, 0xbb, 0x01, 0xfb, 0x55, 0x56, 0xe3, 0x2d, 0xf6, 0x5c, 0xe3, 0x4d, 0x26,
+	0xfb, 0x4e, 0x77, 0xe3, 0x2a, 0x64, 0x95, 0xe9, 0x95, 0x44, 0x95, 0x1a, 0x88, 0x1e, 0x8b, 0x62,
+	0x44, 0x51, 0x2f, 0x71, 0x50, 0xaf, 0xdf, 0x6e, 0xb4, 0xab, 0xbb, 0xba, 0xb3, 0xba, 0x6a, 0xf8,
+	0x62, 0x2a, 0xdb, 0x64, 0xdc, 0x59, 0x71, 0x2c, 0xc1, 0xbb, 0xe2, 0x42, 0xb6, 0x48, 0x4f, 0x60,
+	0xef, 0x2b, 0x6f, 0x07, 0x0c, 0x48, 0x44, 0x12, 0xc2, 0x6c, 0x41, 0xdf, 0xc0, 0xa1, 0x6e, 0x04,
+	0x2a, 0xcd, 0x85, 0x0c, 0x9c, 0x88, 0x24, 0x2e, 0x7b, 0x02, 0x68, 0x0e, 0x07, 0xf8, 0x0d, 0x85,
+	0x6c, 0x79, 0x1f, 0xb8, 0x11, 0x49, 0x8e, 0xf2, 0x57, 0xe9, 0xa6, 0x9b, 0xb4, 0x9c, 0x4e, 0xd9,
+	0x23, 0x2f, 0x1e, 0x00, 0xae, 0x1b, 0x81, 0x57, 0xd8, 0x37, 0xa8, 0xe8, 0x19, 0x78, 0x2d, 0xaf,
+	0xb0, 0x55, 0x01, 0x89, 0xdc, 0xe4, 0x28, 0x7f, 0xb9, 0xad, 0xbf, 0x18, 0xcf, 0xce, 0xe7, 0xf7,
+	0xbf, 0x4e, 0x67, 0x6c, 0x22, 0xd2, 0x77, 0xb0, 0xaf, 0x8c, 0x65, 0x15, 0x38, 0x46, 0x73, 0xb2,
+	0xad, 0xb1, 0xf3, 0x4c, 0xa2, 0x07, 0x6a, 0x7c, 0x06, 0x7b, 0xa6, 0x19, 0xa5, 0x30, 0xbf, 0xe5,
+	0xc2, 0x8e, 0x79, 0xc8, 0xcc, 0xfe, 0x69, 0x76, 0xc7, 0x80, 0xb6, 0x88, 0xdf, 0x83, 0x77, 0x61,
+	0xaf, 0xfc, 0x7f, 0x97, 0xf1, 0x0f, 0x02, 0xc7, 0x06, 0xff, 0xc8, 0xf5, 0x62, 0x89, 0x3d, 0x2d,
+	0x60, 0x3e, 0xfe, 0x9a, 0xb9, 0xf7, 0x45, 0x7e, 0xba, 0xa3, 0xc3, 0xc4, 0x4c, 0xaf, 0x57, 0x12,
+	0x99, 0x21, 0x3f, 0x9a, 0x75, 0x76, 0x99, 0x75, 0x37, 0xcd, 0x26, 0x30, 0x1f, 0x75, 0xd4, 0x03,
+	0xa7, 0xbc, 0xf4, 0x67, 0x74, 0x1f, 0xdc, 0x4f, 0xe5, 0xa5, 0x4f, 0x46, 0x80, 0x95, 0xbe, 0x63,
+	0x00, 0x56, 0xfa, 0x6e, 0x7c, 0x07, 0x07, 0x0f, 0xdf, 0xf2, 0x9c, 0xe7, 0xdf, 0x7a, 0xab, 0xdd,
+	0x39, 0x71, 0xff, 0xc9, 0xc9, 0x79, 0x70, 0xbf, 0x0e, 0xc9, 0xcf, 0x75, 0x48, 0x7e, 0xaf, 0x43,
+	0xf2, 0xfd, 0x4f, 0x38, 0xfb, 0xec, 0xd9, 0x20, 0x57, 0x9e, 0x89, 0x61, 0xf1, 0x37, 0x00, 0x00,
+	0xff, 0xff, 0x0f, 0xe2, 0x32, 0xb7, 0x06, 0x03, 0x00, 0x00,
 }
